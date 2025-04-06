3317d5945fa012e081a4262abd77fb67756eb591
pingguomc <141195321+pingguomc@users.noreply.github.com>
2025-04-06 16:34:01 +0800

    Initial commit

---
+++ docs/intro.md
@@ -0,0 +1,38 @@
+---
+sidebar_position: 1
+---
+
+# Docusaurus 插件文档模板
+
+跟着模板走, 文档不用愁
+
+:::tip
+aaaa啊啊
+:::
+
+:::note
+aaaa啊啊
+:::
+
+:::warning
+aaaa啊啊
+:::
+
+:::danger
+aaaa啊啊
+:::
+
+> [!NOTE]  
+> Highlights information that users should take into account, even when skimming.
+
+> [!TIP]
+> Optional information to help a user be more successful.
+
+> [!IMPORTANT]  
+> Crucial information necessary for users to succeed.
+
+> [!WARNING]  
+> Critical content demanding immediate user attention due to potential risks.
+
+> [!CAUTION]
+> Negative potential consequences of an action.
---
+++ docusaurus.config.js
@@ -0,0 +1,152 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: '红星游戏社区 维基',
+  tagline: 'Docusaurus 插件文档模板',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://pingguomc.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/Wiki/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'pingguomc', // Usually your GitHub org/user name.
+  projectName: 'Wiki', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans', 'en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+          editUrl:
+            'https://github.com/pingguomc/Wiki/blob/main'
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Wiki-Template',
+        logo: {
+          alt: 'Logo',
+          src: 'img/logo.svg',
+        },
+        hideOnScroll: false,
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: '开始',
+          },
+          // 搜索框
+          {
+            type: 'search',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/pingguomc/Wiki',
+            label: 'GitHub',
+            position: 'right',
+          },
+          {
+            type: 'localeDropdown',
+            position: 'right',
+          },
+        ],
+      },
+      // 底部链接
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: '文档',
+            items: [
+              {
+                label: '开始',
+                to: '/intro',
+              },
+            ],
+          },
+          {
+            title: '交流',
+            items: [
+              {
+                label: 'QQ群',
+                href: 'https://github.com/pingguomc/Wiki',
+              },
+            ],
+          },
+          {
+            title: '文档仓库',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/pingguomc/Wiki',
+              },
+            ],
+          },
+        ],
+        // 底部版权信息
+        copyright: `Copyright © ${new Date().getFullYear()} RedStarMC Community, All Rights Reserved.`,
+      },
+      // 深浅主题
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      // 颜色随系统切换
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
+    }),
+
+    themes: [
+      [
+        require.resolve("@easyops-cn/docusaurus-search-local"),
+        {
+          hashed: true,
+          language: ["en", "zh"],
+          highlightSearchTermsOnTargetPage: true,
+          explicitSearchResultPath: true,
+          indexBlog: false,
+          docsRouteBasePath: "/"
+        },
+      ],
+    ],
+};
+
+module.exports = config;
